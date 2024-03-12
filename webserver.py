ad3505c67869f9eb7de6fb42f00d086735c4b30e
donnerbaer <45535297+donnerbaer@users.noreply.github.com>
2024-03-12 06:55:21 +0100

    change to database handler & fetching
    changed to
    + database handler
    + config handler
    + helper.fetch_reddit_user
    
    add function
    + if the annotator saved the annotation and than (re-/)fetches the user_id, the annotator will be redirect to own annotation

--- webserver.py
+++ webserver.py
@@ -1,13 +1,19 @@
 from bottle import route, run, default_app, debug
 from bottle import get, static_file, template, error, redirect, request
-from helper import fetch_user_metadata
+# from helper import fetch_user_metadata
+from helper.fetch_reddit_user import *
 import sqlite3
-import configparser
+
+from app.config import *
+from app.database import *
 
 
-config = configparser.ConfigParser()
-config.read('config.ini')
+config = Config().get_config()
 database_path = config['DATABASE']['FILE']
+db = Database()
+connection = db.get_connection()
+cursor = db.get_cursor()
+
 
 
 # #####################   HELPER   #####################
@@ -28,11 +34,7 @@ def parameter_split(parameter:str) -> dict:
 
 
 def is_in_database(annotator:str, user_id:str) -> bool:
-    connection = sqlite3.connect(database_path)
-    cursor = connection.cursor()
     result = cursor.execute("""SELECT annotator FROM reddit_bots WHERE annotator = ? and user_id = ?""", (annotator, user_id)).fetchone()
-    cursor.close()
-    connection.close()
     if result is None:
         return False
     return True
@@ -44,8 +46,6 @@ def is_in_database(annotator:str, user_id:str) -> bool:
 
 @route('/')
 def index():
-    connection = sqlite3.connect(database_path)
-    cursor = connection.cursor()
     data = {}
     data['annotator_numbers'] = cursor.execute("""
         SELECT DISTINCT annotator, COUNT(annotator) 
@@ -54,15 +54,11 @@ def index():
         """).fetchall()
     number_not_annotated = cursor.execute("""SELECT COUNT(DISTINCT user_id) FROM reddit_bots """).fetchone()
     data['number_not_annotated'] = number_not_annotated[0]
-    cursor.close()
-    connection.close()
     return template('index',data=data)
 
 
 @route('/random')
 def random():
-    connection = sqlite3.connect(database_path)
-    cursor = connection.cursor()
     user_id = cursor.execute("""
         SELECT 
 	        user_id
@@ -75,16 +71,12 @@ def random():
                 ) LIMIT 1
         """).fetchone()
     user_id = user_id[0]
-    cursor.close()
-    connection.close()
     redirect('/annotation/{}'.format(user_id))
 
 
 @route('/next')
 def next():
     parameter = parameter_split(request.query_string)
-    connection = sqlite3.connect(database_path)
-    cursor = connection.cursor()
     annotator, user_id = cursor.execute("""                        
         SELECT 
             annotator,
@@ -97,8 +89,6 @@ def next():
                 WHERE annotator IS NOT NULL
                 )
         """).fetchone()
-    cursor.close()
-    connection.close()
     if parameter.get('annotator') is not None:
         redirect(f'/annotation/{user_id}?annotator={parameter.get('annotator')}')
     redirect(f'/annotation/{user_id}')
@@ -138,12 +128,8 @@ def save():
             INSERT OR REPLACE INTO reddit_bots VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);
     """
 
-    connection = sqlite3.connect(database_path)
-    cursor = connection.cursor()
     cursor.execute(query_insert, insert_data)
     connection.commit()
-    cursor.close()
-    connection.close()
 
     annotator = request.forms.get('annotation_marker')
     if request.forms.get('annotator') is not None:
@@ -157,19 +143,13 @@ def save():
 
 @route('/database')
 def database():
-    connection = sqlite3.connect(database_path)
-    cursor = connection.cursor()
     data = cursor.execute("""SELECT * FROM reddit_bots""").fetchall()
-    cursor.close()
-    connection.close()
     return template('database', data=data, activeNav="database", annotator_marker=None)
 
 
 @route('/database/not_annotated')
 @route('/database/not_annotated/<annotator>')
 def database(annotator:str = None):
-    connection = sqlite3.connect(database_path)
-    cursor = connection.cursor()
     if annotator is None:
         data = cursor.execute("""SELECT * FROM reddit_bots WHERE annotator IS NULL""").fetchall()
     else:
@@ -177,35 +157,25 @@ def database(annotator:str = None):
                 FROM reddit_bots 
                 WHERE annotator = ?
                 ) """, (annotator,)).fetchall()
-    cursor.close()
-    connection.close()
     return template('database', data=data, activeNav="database/not_annotated", annotator_marker=annotator)
 
 
 @route('/database/has_annotation')
 @route('/database/has_annotation/<annotator>')
 def database(annotator:str = None):
-    connection = sqlite3.connect(database_path)
-    cursor = connection.cursor()
     if annotator is None:
         data = cursor.execute('''SELECT * FROM reddit_bots WHERE annotator IS NOT NULL OR annotator != "" ''').fetchall()
     else:
         data = cursor.execute('''SELECT * FROM reddit_bots WHERE annotator = ? ''', (annotator,)).fetchall()
-    cursor.close()
-    connection.close()
     return template('database', data=data, activeNav="database/has_annotation", annotator_marker=annotator)
 
 
 @route('/database/has_fetched')
 def database():
-    connection = sqlite3.connect(database_path)
-    cursor = connection.cursor()
     data = cursor.execute('''
                 SELECT * FROM reddit_bots WHERE 
                 was_fetched = 1
             ''').fetchall()
-    cursor.close()
-    connection.close()
     return template('database', data=data, activeNav="database/has_annotation", annotator_marker=None)
 
 
@@ -224,14 +194,10 @@ def new():
 @route('/annotation/<user_id>/<annotator>')
 def annotation(user_id:str = '', annotator = None):
     parameter = parameter_split(request.query_string)
-    connection = sqlite3.connect(database_path)
-    cursor = connection.cursor()
     if annotator is not None and annotator != 'None':
         data = cursor.execute("""SELECT * FROM reddit_bots WHERE user_id = ? and annotator = ? LIMIT 1""",(user_id, annotator)).fetchone()
     else:
         data = cursor.execute("""SELECT * FROM reddit_bots WHERE user_id = ? LIMIT 1""",(user_id, )).fetchone()
-    cursor.close()
-    connection.close()
     annotation_maker = parameter.get('annotator')
     return template('annotation', data=data, annotator_marker=annotation_maker, anno_type='annotation')
 
@@ -241,13 +207,13 @@ def annotation(user_id:str = '', annotator = None):
 def fetch(user_id:str, annotator:str = None):
     parameter = parameter_split(request.query_string)
 
-    user_metadata = fetch_user_metadata.fetch_user_metadata(user_id)
-    success = False
-    if user_metadata != -1:
-        success = fetch_user_metadata.save_metadata_to_json(user_id, user_metadata)
-    
-    connection = sqlite3.connect("database/database.db")
-    cursor = connection.cursor()
+    fetched = False
+    user_metadata = fetch_user_metadata(user_id)
+    if isinstance(user_metadata, dict):
+        success = save_metadata_to_json(user_id, user_metadata)
+        fetched = True
+        print(f'Fetching metadata successful: {success}')
+
     query = """
                 UPDATE reddit_bots 
                 SET 
@@ -255,11 +221,11 @@ def fetch(user_id:str, annotator:str = None):
                 WHERE
                     user_id = ?
             """
-    cursor.execute(query, (success, user_id))
+    cursor.execute(query, (fetched, user_id))
     connection.commit()
-    cursor.close()
-    connection.close()
-    redirect('/annotation/{}?annotator={}'.format(user_id, parameter.get('annotator')))
+    if annotator is None or annotator == '':
+        redirect('/annotation/{}?annotator={}'.format(user_id, parameter.get('annotator')))
+    redirect('/annotation/{}/{}'.format(user_id, annotator))
 
 
 
@@ -292,16 +258,12 @@ def search_result():
 
     # print(search_parameter)
 
-    # connection = sqlite3.connect(database_path)
-    # cursor = connection.cursor()
     # data = cursor.execute("""
     #                       SELECT * 
     #                       FROM reddit_bots 
     #                       WHERE annotator IS NULL 
                           
     #                       """,search_parameter).fetchall()
-    # cursor.close()
-    # connection.close()
     
     # return template('database', data=data, activeNav="database")
     redirect('/database')
@@ -351,3 +313,5 @@ def json(filepath):
 
 if __name__ == '__main__':
     run(host="localhost", port="80", reloader=True, debug=True)
+    db.close()
+
