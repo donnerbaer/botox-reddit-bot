17b41f4e8df1207335e264b5e478cba4efadd0b8
donnerbaer <45535297+donnerbaer@users.noreply.github.com>
2024-03-20 23:08:02 +0100

    Update fetch_reddit_user.py
    add fetch several pages

--- helper/fetch_reddit_user.py
+++ helper/fetch_reddit_user.py
@@ -11,7 +11,7 @@ from app import config, database
 try:
     config = config.Config().get_config()
 except:
-    print("ERROR: helper.fetch_reddit_user.py: pronlem with config.ini")
+    print("ERROR: helper.fetch_reddit_user.py: problem with config.ini")
     os._exit(0)
 
 try:
@@ -19,7 +19,7 @@ try:
     __connection = __db.get_connection()
     __cursor = __db.get_cursor()
 except:
-    print("ERROR: helper.fetch_reddit_user.py: pronlem with config.ini")
+    print("ERROR: helper.fetch_reddit_user.py: database problem with config.ini")
     os._exit(0)
 
 
@@ -53,24 +53,23 @@ def fetch_user_metadata(username:str) -> dict|str:
     Returns:
         dict|str: _description_
     """
-    # Reddit API endpoint for user information
+    limit = 100
+
     user_url = f"https://www.reddit.com/user/{username}/about.json"
-    # Reddit API endpoint for user's recent posts
     posts_url = f"https://www.reddit.com/user/{username}/submitted.json"
-    # Reddit API endpoint for user's recent comments
     comments_url = f"https://www.reddit.com/user/{username}/comments.json"
     
-    # Send GET request to fetch user information
+    posts_url += f"?sort=new&limit={limit}"
+    comments_url += f"?sort=new&limit={limit}"
+
+
     user_response = requests.get(user_url, headers={"User-Agent": "YOUR_APP_NAME"})
     
-    # Check if the request was successful
     if user_response.status_code == 200:
-        # Parse JSON response for user information
         
         if ALLOW_FETCH_FULL_JSON == 'yes':
             user_metadata = user_response.json()
         else: 
-        # Extract relevant user metadata
             user_data = user_response.json()['data']
             created_utc = format_timestamp(user_data.get('created_utc'))
             user_metadata = {
@@ -86,62 +85,83 @@ def fetch_user_metadata(username:str) -> dict|str:
             }
         
 
-        # Send GET request to fetch user's recent posts
-        posts_response = requests.get(posts_url, headers={"User-Agent": "YOUR_APP_NAME"})
-        # Parse JSON response for user's recent posts
-        if posts_response.status_code == 200:
-                  
-            if ALLOW_FETCH_FULL_JSON == 'yes':
-                recent_posts_metadata = posts_response.json()
-            else: 
-                posts_data = posts_response.json()['data']['children']
-                recent_posts_metadata = []
-                for post in posts_data:
-                    post_data = post.get('data')
-                    created_utc = format_timestamp(post_data.get('created_utc'))
-
-                    post_metadata = {
-                        'title': post_data.get('title'),
-                        'created_utc': created_utc,
-                        'permalink': post_data.get('permalink'),
-                        'num_comments': post_data.get('num_comments'),
-                        'score': post_data.get('score'),
-                        'subreddit': post_data.get('subreddit'),
-                        'is_self': post_data.get('is_self'),
-                        'domain': post_data.get('domain')
-                    }
-                    recent_posts_metadata.append(post_metadata)
-
-            user_metadata['recent_posts'] = recent_posts_metadata
-        
-        # Send GET request to fetch user's recent comments
-        comments_response = requests.get(comments_url, headers={"User-Agent": "YOUR_APP_NAME"})
-        # Parse JSON response for user's recent comments
-        if comments_response.status_code == 200:
-            if ALLOW_FETCH_FULL_JSON == 'yes':
-                recent_comments_metadata = comments_response.json()
-            else: 
-
-                comments_data = comments_response.json()['data']['children']
-                # Extract metadata of recent comments
-                recent_comments_metadata = []
-                for comment in comments_data:
-                    comment_data = comment.get('data')
-                    created_utc = format_timestamp(comment_data.get('created_utc'))
+            pages:int = 10
+            posts_url_page:str = ""
+            recent_posts_metadata = []
+            for page in range(pages):
+                if page != 0:
+                    after = posts_response.json().get('data')
+                    if after == None:
+                        break
+                    after=after.get('after')
+                    if after == None:
+                        break
+                    posts_url_page = f"&after={after}&count={len(recent_posts_metadata)}"
+                posts_response = requests.get((posts_url + posts_url_page), headers={"User-Agent": "YOUR_APP_NAME"})
                 
-                    comment_metadata = {
-                        'body': comment_data.get('body'),
-                        'created_utc': created_utc,
-                        'link_id': comment_data.get('link_id'),
-                        'score': comment_data.get('score'),
-                        'subreddit': comment_data.get('subreddit'),
-                        'permalink': comment_data.get('permalink')
-                    }
-                    recent_comments_metadata.append(comment_metadata)
+                if posts_response.status_code == 200:
+                        
+                    if ALLOW_FETCH_FULL_JSON == 'yes':
+                        recent_posts_metadata = posts_response.json()
+                    else: 
+                        posts_data = posts_response.json()['data']['children']
+                        recent_posts_metadata = []
+                        for post in posts_data:
+                            post_data = post.get('data')
+                            created_utc = format_timestamp(post_data.get('created_utc'))
 
-            user_metadata['recent_comments'] = recent_comments_metadata
+                            post_metadata = {
+                                'title': post_data.get('title'),
+                                'created_utc': created_utc,
+                                'permalink': post_data.get('permalink'),
+                                'num_comments': post_data.get('num_comments'),
+                                'score': post_data.get('score'),
+                                'subreddit': post_data.get('subreddit'),
+                                'is_self': post_data.get('is_self'),
+                                'domain': post_data.get('domain')
+                            }
+                            recent_posts_metadata.append(post_metadata)
+
+                    user_metadata['recent_posts'] = recent_posts_metadata
+        
+
+        comments_url_page:str = ""
+        for page in range(pages):
+            if page != 0:
+                after = comments_response.json().get('data')
+                if after == None:
+                    break
+                after = after.get('after')
+                if after == None:
+                    break
+                comments_url_page = f"&after={after}&count={len(recent_comments_metadata)}"
+            comments_response = requests.get(comments_url + comments_url_page, headers={"User-Agent": "YOUR_APP_NAME"})
+
+            if comments_response.status_code == 200:
+                if ALLOW_FETCH_FULL_JSON == 'yes':
+                    recent_comments_metadata = comments_response.json()
+                else: 
+
+                    comments_data = comments_response.json()['data']['children']
+                    # Extract metadata of recent comments
+                    recent_comments_metadata = []
+                    for comment in comments_data:
+                        comment_data = comment.get('data')
+                        created_utc = format_timestamp(comment_data.get('created_utc'))
+                    
+                        comment_metadata = {
+                            'body': comment_data.get('body'),
+                            'created_utc': created_utc,
+                            'link_id': comment_data.get('link_id'),
+                            'score': comment_data.get('score'),
+                            'subreddit': comment_data.get('subreddit'),
+                            'permalink': comment_data.get('permalink')
+                        }
+                        recent_comments_metadata.append(comment_metadata)
+                user_metadata['recent_comments'] = recent_comments_metadata
 
         return user_metadata
+    
     else:
         return f'ERROR {user_response.status_code}'
         
@@ -175,6 +195,7 @@ def process_fetch(username:str) -> None:
     
 
     time_sleep = (random.random() * 4) + 1
+    # time_sleep = 0
     print(f'Pausing for: {time_sleep} sec')
     time.sleep(time_sleep)
 
